0e9d8308ab5822a95ee5894f59cf121d3e8cd397
=blindpaper01 <blindpaper01@gmail.com>
2018-01-04 23:44:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 911_floor
+This is the blind review code for the paper. It is not polished for open-source but contains all the logs and functionality in the paper
+
+# Install package
+```bash
+pip install -e .
+```
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='floor',
+      version='0.0.1',
+      description='Find a floor number',
+      author='---',
+      author_email='---',
+      url='https://github.com/blindpaper01/paper_FMhXSlwRYpUtuchTv',
+      install_requires=['numpy', 'test_tube'],
+      packages=['floor']
+      )
